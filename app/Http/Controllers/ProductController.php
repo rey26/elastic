b98a5af3db05e4c277e34a99859aaf17b1c97f0a
Matej Malicky <matej.malicky@gmail.com>
2020-12-03 11:07:47 +0100

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Routing\Controller as BaseController;
+
+class ProductController extends BaseController
+{
+    /**
+     * @param string $id
+     * @return string
+     */
+    public function detail($id)
+    {
+        
+    }
+}
